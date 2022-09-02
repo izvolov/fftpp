61f035af864ccab5525bf90f82e0dfa5d7d52155
Дмитрий Изволов <dmitriy@izvolov.ru>
2022-09-02 23:47:54 +0300

    Декомпозиция

--- include/fftpp/utility/bit_reversal_permutation.hpp
+++ include/fftpp/utility/bit_reversal_permutation.hpp
@@ -10,12 +10,31 @@
 
 namespace fftpp
 {
+    namespace detail
+    {
+        template <std::forward_iterator I, std::sentinel_for<I> S>
+        constexpr I bit_reversal_permutation_iteration (I chunk_first, S chunk_last)
+        {
+            std::for_each(chunk_first, chunk_last,
+                [] (auto & x)
+                {
+                    x *= 2;
+                });
+
+            return
+                std::transform(chunk_first, chunk_last, chunk_last,
+                    [] (auto x)
+                    {
+                        return ++x;
+                    });
+        }
+    }
+
     // https://en.wikipedia.org/wiki/Bit-reversal_permutation
     template <std::forward_iterator I, std::sentinel_for<I> S>
         requires(std::integral<std::iter_value_t<I>>)
     constexpr I bit_reversal_permutation (I indices_first, S indices_last)
     {
-
         if (indices_first != indices_last)
         {
             *indices_first = std::iter_value_t<I>{0};
@@ -23,18 +42,7 @@ namespace fftpp
 
             while (end != indices_last)
             {
-                std::for_each(indices_first, end,
-                    [] (auto & x)
-                    {
-                        x *= 2;
-                    });
-
-                end =
-                    std::transform(indices_first, end, end,
-                        [] (auto x)
-                        {
-                            return ++x;
-                        });
+                end = detail::bit_reversal_permutation_iteration(indices_first, end);
             }
 
             return end;
