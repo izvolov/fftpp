2eba0bdfb462a6ec1954eeb03a229d80d09d5145
Дмитрий Изволов <dmitriy@izvolov.ru>
2023-10-14 17:46:32 +0600

    Возможность указывать преобразование для диапазона корней из единицы

--- include/fftpp/detail/fft_impl.hpp
+++ include/fftpp/detail/fft_impl.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <fftpp/detail/butterfly.hpp>
+#include <fftpp/utility/first_of.hpp>
 
 #include <cassert>
 #include <concepts>
@@ -12,9 +13,11 @@ namespace fftpp::detail
     <
         std::random_access_iterator I,
         std::integral D = std::iter_difference_t<I>,
-        std::random_access_iterator J
+        std::random_access_iterator J,
+        std::regular_invocable<J, D> P = first_of_t
     >
-    void fft_impl (I first, D size, J w_nk)
+        requires(subscriptable<std::invoke_result_t<P, J, D>>)
+    void fft_impl (I first, D size, J w_nk, P proj = {})
     {
         assert(size >= 0);
 
@@ -24,7 +27,7 @@ namespace fftpp::detail
             {
                 auto begin = first + k;
                 auto end = begin + n / 2;
-                multi_butterfly(begin, end, end, w_nk);
+                multi_butterfly(begin, end, end, proj(w_nk, n));
             }
 
             using std::advance;
