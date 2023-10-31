8652c39bc43840f26e01fe2ef71b5c14fbe1e9b5
Дмитрий Изволов <dmitriy@izvolov.ru>
2023-11-01 00:40:22 +0600

    refs #7 Предупреждение об отсутствии doctest не нужно
    Поскольку мы его тут же выкачиваем из интернета.

--- cmake/doctest.cmake
+++ cmake/doctest.cmake
@@ -1,7 +1,7 @@
 set(MYLIB_DOCTEST_VERSION 2.4.8)
 set(MYLIB_DOCTEST_REPOSITORY https://github.com/doctest/doctest.git)
 
-find_package(doctest ${MYLIB_DOCTEST_VERSION})
+find_package(doctest ${MYLIB_DOCTEST_VERSION} QUIET)
 
 if (doctest_FOUND)
     message(STATUS "Найден doctest ${doctest_VERSION}: ${doctest_DIR}")
